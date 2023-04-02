01d540fb29ccc900212a19c93d7e62712c8f9730
ptpofficialxd <aekkungzaza@gmail.com>
2023-04-02 16:50:14 +0700

    remove all init

--- public/js/signup-success.js
+++
@@ -1,9 +0,0 @@
-const contBtn = document.getElementById("contBtn");
-contBtn.addEventListener("click", function() {
-  location.href = "login";
-});
-
-const contBtn2 = document.getElementById("contBtn2");
-contBtn2.addEventListener("click", function() {
-  location.href = "ordermap";
-});
